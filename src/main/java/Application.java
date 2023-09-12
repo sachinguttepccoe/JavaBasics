51dbff4eb8489a737a130d8cf912e9f4aea69c3e
Sachin Gutte <85564484+sachinguttepccoe@users.noreply.github.com>
2023-09-12 15:47:36 +0200

    Update Application.java
    removed object creation since this is static method

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,9 +4,8 @@ import com.javabasics.services.Welcome;
 public class Application {
     public static void main(String[] args) {
 
-        // Simple first java program
-        Welcome welcomeObj = new Welcome();
-        welcomeObj.displayMessage("Hello, World.");
+        // Simple first java program to prints data on console 
+           Welcome.displayMessage("Hello, World.");
 
         // standard input and output program with scanner
         JavaStdIO javaStdIOObj = new JavaStdIO(System.in, System.out);
